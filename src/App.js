f99c8fdd51489c4c168a547931ef37246bc8a57a
samith thilanka <samith@rhino-partners.com>
2019-10-03 14:51:06 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+
+import { Router, browserHistory, Route, Link } from 'react-router';
+
+import './App.css';
+
+const NavBar = () => (
+
+  <div className="navbar">
+
+    <Link to="/">Feed</Link>
+
+    <Link to="/profile">Profile</Link>
+
+  </div>
+
+);
+
+const Template = ({ title }) => (
+
+  <div>
+
+    <NavBar />
+
+    <p className="page-info">
+
+      This is the {title} page.
+      <img src="/icon-512x512.png" alt="Smiley face"  /> 
+    </p>
+
+  </div>
+
+);
+
+const Feed = (props) => (
+
+  <Template title="Feed"/>
+
+);
+
+const Profile = (props) => (
+
+  <Template title="Profile"/>
+
+);
+
+class App extends Component {
+
+  render() {
+
+    return (
+
+      <Router history={browserHistory}>
+
+        <Route path="/" component={Feed}/>
+
+        <Route path="/profile" component={Profile}/>
+
+      </Router>
+
+    );
+
+  }
+
+}
+
+export default App;
